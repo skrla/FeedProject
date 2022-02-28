5493048c9f9b836b9c6fe40506d15be176f098b3
skrla <skrlama@gmail.com>
2022-03-01 00:17:13 +0100

    Added getter for other models.

--- app/src/main/java/skrla/feedprocject/model/data/api/FeedApi.kt
+++ app/src/main/java/skrla/feedprocject/model/data/api/FeedApi.kt
@@ -2,11 +2,24 @@ package skrla.feedprocject.model.data.api
 
 import retrofit2.Call
 import retrofit2.http.GET
-import skrla.feedprocject.model.data.models.FeedApiData
+import retrofit2.http.Path
+import skrla.feedprocject.model.data.models.*
 
 interface FeedApi {
 
     @GET("feed?page=1&sport=football")
     fun getFeed() : Call<List<FeedApiData>>
+
+    @GET("athlete")
+    fun getAthlete() : Call<List<AthleteApi>>
+
+    @GET("country")
+    fun getCountry() : Call<List<CountryApi>>
+
+    @GET("sport")
+    fun getSport() : Call<List<SportApi>>
+
+    @GET("sport/{id}/group")
+    fun getSportGroup(@Path("id") id: String): Call<List<SportGroupApi>>
 
 }
