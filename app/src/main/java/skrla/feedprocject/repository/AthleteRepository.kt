e074c0186b71b72d1343344b6b1809ffa23cf294
skrla <skrlama@gmail.com>
2022-03-01 00:11:07 +0100

    Added repository's for other models

---
+++ app/src/main/java/skrla/feedprocject/repository/AthleteRepository.kt
@@ -0,0 +1,42 @@
+package skrla.feedprocject.repository
+
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import skrla.feedprocject.model.data.api.RetrofitClient
+import skrla.feedprocject.model.data.models.AthleteApi
+
+class AthleteRepositoryImp(val application: Application) : AthleteRepository {
+
+    val athleteRepo = MutableLiveData<List<AthleteApi>>()
+
+    override fun getAthlete(): LiveData<List<AthleteApi>> {
+        RetrofitClient.retrofitFeed.getAthlete().enqueue(object : Callback<List<AthleteApi>> {
+
+            override fun onResponse(
+                call: Call<List<AthleteApi>>,
+                response: Response<List<AthleteApi>>
+            ) {
+                if(response.isSuccessful) {
+                    athleteRepo.value = response.body()!!
+                } else {
+                    Toast.makeText(application, "Error loading feed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<AthleteApi>>, t: Throwable) {
+                Toast.makeText(application, "Error", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        return athleteRepo
+    }
+}
+
+interface AthleteRepository {
+    fun getAthlete(): LiveData<List<AthleteApi>>
+}
---
+++ app/src/main/java/skrla/feedprocject/repository/CountryRepository.kt
@@ -0,0 +1,42 @@
+package skrla.feedprocject.repository
+
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import skrla.feedprocject.model.data.api.RetrofitClient
+import skrla.feedprocject.model.data.models.CountryApi
+
+class CountryRepositoryImp(val application: Application) : CountryRepository {
+
+    val countryRepo = MutableLiveData<List<CountryApi>>()
+
+    override fun getCountry() : LiveData<List<CountryApi>> {
+        RetrofitClient.retrofitFeed.getCountry().enqueue(object : Callback<List<CountryApi>> {
+            override fun onResponse(
+                call: Call<List<CountryApi>>,
+                response: Response<List<CountryApi>>
+            ) {
+                if (response.isSuccessful) {
+                    countryRepo.value = response.body()!!
+                } else {
+                    Toast.makeText(application, "Error loading feed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<CountryApi>>, t: Throwable) {
+                Toast.makeText(application, "Error", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+        return countryRepo
+    }
+}
+
+interface CountryRepository {
+    fun getCountry() : LiveData<List<CountryApi>>
+}
