49d588db5ef57b372251f8e23dd0c8f1a55e77ef
skrla <skrlama@gmail.com>
2022-03-01 00:16:45 +0100

    Added more data in data class.

--- app/src/main/java/skrla/feedprocject/model/data/models/FeedApiData.kt
+++ app/src/main/java/skrla/feedprocject/model/data/models/FeedApiData.kt
@@ -1,15 +1,16 @@
 package skrla.feedprocject.model.data.models
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 import java.util.*
 
-@Parcelize
 data class FeedApiData(
     val id: String,
     val createdAt: Date,
     val createdBefore: String,
+    val author: AuthorApi,
+    val sportGroup: SportGroupApi,
+    val video: VideoApi,
     val description: String,
+    val athlete: AthleteApi,
     val bookmarked: String,
     val views: String
-) : Parcelable
+)
