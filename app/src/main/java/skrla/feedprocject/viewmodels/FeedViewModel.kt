5d75d688445e22c33a7b0a28f74fd7fa787a3887
skrla <skrlama@gmail.com>
2022-03-05 23:42:19 +0100

    Connected Athlete repository with view model class

--- app/src/main/java/skrla/feedprocject/viewmodels/FeedViewModel.kt
+++ app/src/main/java/skrla/feedprocject/viewmodels/FeedViewModel.kt
@@ -1,33 +1,35 @@
 package skrla.feedprocject.viewmodels
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import skrla.feedprocject.databinding.FragmentFeedBinding
+import skrla.feedprocject.model.data.models.AthleteApi
 import skrla.feedprocject.model.data.models.FeedApiData
+import skrla.feedprocject.repository.AthleteRepositoryImp
 import skrla.feedprocject.repository.FeedRepositoryImp
 
 class FeedViewModel(application: Application) : AndroidViewModel(application) {
 
-    private var _binding: FragmentFeedBinding? = null
-    private val binding get() = _binding!!
-
     private val repositoryFeed = FeedRepositoryImp(application)
+    private val repositoryAthlete = AthleteRepositoryImp(application)
 
     val feedApiData: LiveData<List<FeedApiData>>
+    val athleteApi: LiveData<List<AthleteApi>>
 
     init {
-        Log.wtf("D:", "wtf")
         feedApiData = repositoryFeed.feedRepo
+        athleteApi = repositoryAthlete.athleteRepo
     }
 
-    fun getFeed() {
+    fun getData() {
         viewModelScope.launch {
-            Thread.sleep(2000)
             repositoryFeed.getFeed()
+            repositoryAthlete.getAthlete()
+            @Suppress("BlockingMethodInNonBlockingContext")
+            Thread.sleep(2000)
         }
     }
+
 }
