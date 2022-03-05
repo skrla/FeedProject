b5f5dbaa84428e4434278a9877782049820c7484
skrla <skrlama@gmail.com>
2022-03-05 23:36:12 +0100

    Made AthleteAdapter for recyclerview.

---
+++ app/src/main/java/skrla/feedprocject/ui/adapters/AthleteAdapter.kt
@@ -0,0 +1,59 @@
+package skrla.feedprocject.ui.adapters
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import skrla.feedprocject.databinding.AthleteDataBinding
+import skrla.feedprocject.model.data.models.AthleteApi
+
+class AthleteAdapter : ListAdapter<AthleteApi, AthleteAdapter.AthleteViewHolder>(DiffCallback) {
+
+    companion object DiffCallback : DiffUtil.ItemCallback<AthleteApi>() {
+        override fun areItemsTheSame(oldItem: AthleteApi, newItem: AthleteApi): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: AthleteApi, newItem: AthleteApi): Boolean {
+            return areItemsTheSame(oldItem, newItem)
+        }
+
+    }
+
+
+    class AthleteViewHolder(private var binding: AthleteDataBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(athlete: AthleteApi) {
+            binding.athlete = athlete
+            binding.executePendingBindings()
+        }
+
+        fun expand() {
+            if (binding.expandableLayout.isVisible) {
+                binding.expandableLayout.visibility = View.GONE
+            } else {
+                binding.expandableLayout.visibility = View.VISIBLE
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AthleteViewHolder {
+        return AthleteViewHolder(
+        AthleteDataBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: AthleteViewHolder, position: Int) {
+        val athleteId = getItem(position)
+        holder.bind(athleteId)
+
+        holder.itemView.setOnClickListener {
+            holder.expand()
+        }
+    }
+}
+
