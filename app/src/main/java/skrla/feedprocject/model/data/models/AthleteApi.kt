66a824800596e877b1f613bce65a9b0e7dba6780
skrla <skrlama@gmail.com>
2022-03-05 23:37:11 +0100

    Changed types in api to String

--- app/src/main/java/skrla/feedprocject/model/data/models/AthleteApi.kt
+++ app/src/main/java/skrla/feedprocject/model/data/models/AthleteApi.kt
@@ -2,11 +2,13 @@ package skrla.feedprocject.model.data.models
 
 import com.google.gson.annotations.SerializedName
 
+
 data class AthleteApi(
-    val id: Int,
-    val age: Int,
+    val id: String,
+    val age: String,
     val name: String,
-    @SerializedName("avatar") val picOfPerson: String,
+    @SerializedName("avatar") val picOfPerson: String?,
     val club: String,
-    val isCelebrity: Boolean
+    val isCelebrity: String,
+    val country: CountryApi
 )
